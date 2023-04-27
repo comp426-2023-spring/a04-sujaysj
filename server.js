d649b972678580ba8e5502c084e9a07a4445980f
sujaysj <sujaysj@gmail.com>
2023-04-27 12:40:28 -0400

    query

--- server.js
+++ server.js
@@ -25,12 +25,20 @@ app.post('/app/rps/play', (req, res, next) => {
     res.status(200).send(rps(req.body.shot()))
 })
 
+app.post('/app/rps/play', (req, res, next) => {
+    res.status(200).send(rps(req.query.shot()))
+})
+
 app.get('/app/rps/play/:shot', (req, res, next) => {
     res.status(200).send(rps(req.params.shot))
 })
 
 app.post('/app/rpsls/play', (req, res, next) => {
     res.status(200).send(rpsls(req.body.shot))
+})
+
+app.post('/app/rpsls/play', (req, res, next) => {
+    res.status(200).send(rpsls(req.query.shot))
 })
 
 app.get('/app/rpsls/play/:shot', (req, res, next) => {
